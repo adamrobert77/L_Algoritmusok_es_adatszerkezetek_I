20863946716d68379d9616482578679f1561089f
adamrobert77 <159339330+adamrobert77@users.noreply.github.com>
2024-10-27 20:54:24 +0100

    Update Solution.java

--- 01_Moho_strategia/Solution.java
+++ 01_Moho_strategia/Solution.java
@@ -1,66 +1,21 @@
 public class Solution {
 
     // Complete the getMinimumCost function below.
-    static int getMinimumCost(int k, int[] c) {
+    static int getMinimumCost(int k, int[] c) { // ahol c = viragok listaarai, k = vasarlok szama
         
         int eredmeny = 0;
-        Arrays.sort(c);
+        Arrays.sort(c); // rendezzuk novekvo sorrendbe a tombot, csak ilyen modon mukodik az algoritmus
         
-        if(k < c.length) {
-        // ESET --> tobb virag van, mint ember
-        // taroljuk: adott szemelyek eddig hany viragot vasaroltak
-        // index = szemely, ertek = ennyi viragot vett eddig
-        
-        int[] ennyi_viragot_vett = new int[k];
+        int[] ennyi_viragot_vett = new int[k]; // felvesszuk a vasarlok szama meretu tombot, minden erteket 0-ra inicializalva
         
         
         for(int i = c.length-1; i>=0; i--) {
-            // ez biztositja, hogy mindig olyan szemelyt valassszunk ki, aki eddig a legkevesebb viragot vasarolta ---> nincs szukseg kulon minimumot keresni a tombben
-            int szemely = i%k;
-            eredmeny += (ennyi_viragot_vett[szemely] + 1)*c[i];
-            ennyi_viragot_vett[szemely] = ennyi_viragot_vett[szemely]+1;
+            int vasarlo = i%k; // ezzel a modszerrel ismetlesesen vegig tudunk menni a vasarlokon, nincs szukseg minimum keresesre - sorban szolgaljuk ki a vasarlokat
+            eredmeny += (ennyi_viragot_vett[vasarlo] + 1)*c[i];
+            ennyi_viragot_vett[vasarlo] = ennyi_viragot_vett[vasarlo]+1;
         }
         
         return eredmeny; 
         
-        } else {
-            
-        // ESET --> legfeljebb annyi virag van, mint ember
-        // menjunk vegig a tombon novekvo sorrendben, adjuk hozza a viragok erteket az eredmeny valtozohoz
-        
-        int i = 0;
-        while(i<k && i<c.length) {
-            eredmeny += c[i];
-            i++;
-        }
-        
-        return eredmeny;
-        }
-        
-        
-        
-        // virag_ara = (eddig_vasarolt_mennyiseg + 1) * listaar
-        
-        // ha (emberek_szama)<=(virag) nullatol felfele szamolunk
-        // egyebkent csokkeno sorrendben
-        
-        // 1. hatarozzuk meg, hogy tobb-e az ember, mint a virag
-        
-        // 2. amennyiben kisebb-egyenlo ember van, mint virag, akkor
-        // az eredmenyt megkapjuk, ha nulla indextol kezdve osszeadjuk
-        // a viragok arait
-        
-        // 3. amennyiben tobb az ember, mint a virag, akkor ar szerint
-        // csokkeno sorrendben elkezdjuk osszeadni a viragok arait
-        // ha vegigmentunk minden emberen, akkor kivalasztjuk azt
-        // a szemelyt, aki eddig a legkebesebb viragot vette, es
-        // ar szerint novekvo sorrendben kezdjuk el megvenni a viragokat
-        // ha elfogyott az osszes virag, visszaterunk a result-szel
-    
-        
-        
-        
-        
-
     }
 }
