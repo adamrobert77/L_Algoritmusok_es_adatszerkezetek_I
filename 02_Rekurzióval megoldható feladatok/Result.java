7f06cdbb5546f2159f6dc2cc3dc0060f84a22e64
adamrobert77 <159339330+adamrobert77@users.noreply.github.com>
2024-11-01 18:16:00 +0100

    Update Result.java

--- 02_Rekurzióval megoldható feladatok/Result.java
+++ 02_Rekurzióval megoldható feladatok/Result.java
@@ -12,64 +12,42 @@ class Result {
     public static String passwordCracker(List<String> passwords, String loginAttempt) {
             // Write your code here
             
-            // legelso sor: tesztesetek szama
-        // masodik sor: a tombben/taroloban talalhato string-ek szama
-        // harmadik sor: loginAttempt, egy string, amely a probalkozasunkat reprezentalja
-
-        // feladat: probaljuk kirakni a taroloban talalhato string-ekbol a loginAttempt string-et
-
-        // a tarolobol egy string-et akarmennyiszer felhasznalhatunk
-        // visszateresi ertek: felhasznalt string-ek megfelelo sorrendben, space-szel elvalasztva
-
-        // elso megkozelites:
-
-        // kezdjunk el vegigiteralni a kapott string-eken
-        // a vizsgalt string-et adott hosszban hasonlitsuk ossze a loginAttempt substring-javel
-        // (nulladik indextol hossz mennyisegu string-et)
-
-        // amennyiben beillesztheto:
-        // mentsuk el a string-et egy result valtozoba -- vagy valami stringbuilder szerusegbe
-        // vagjuk ki a talalt string-et a loginAttempt-bol -- tehat masoljuk at a loginAttempt substring-jet egy masik valtozoba
-        // az uj valtozora hivjuk meg ujra a fugvenyt, itt tortenik meg a rekurzio
-
-        // amennyiben nem beillesztheto, es van meg a taroloban mas string is:
-        // nezzuk meg a kovetkezo string-et, es igy tovabb
-
-        // ha mar nincs string, es egyik sem volt beillesztheto:
-        // visszaterunk a WRONG PASSWORD string-gel
+          
         
-        
+        // Amennyiben a loginAttempt ures, terjunk vissza ures String-gel --- ez kelleni fog a megfelelo rekurziv megoldashoz
             if(loginAttempt.isEmpty()) {
             return "";
         }
         
       
-        // csak azokkal foglalkozzunk, ami benne lehet a loginAttempt-ben, ezzel javitva gyorsasagon
+        // Csak azokon iteralunk vegig, amelyek egyaltalan megtalalhatoak a loginAttempts-ben, tobbi elem redundans
         List<String> validPasswords = new ArrayList<>();
         for (String password : passwords) {
                 if (loginAttempt.contains(password)) {
                     validPasswords.add(password);
+            }
         }
-    }
         
-          // Hozzuk letre az iteratort
+        // Hozzuk letre az iteratort
         Iterator<String> iterator = validPasswords.iterator();
       
-        // Keressuk meg azt a reszt, amely egyezik a loginAttempt elejevel
+        // Iteraljunk vegig a megfelelo substring-eken, megoldas darakban
         while(iterator.hasNext()) {
             String passSubString = iterator.next();
-            
-    
-            
-            // ha a vizsgalt stringgel kezdodik, es nem kell tobb darakbat talalnunk
+        
+            // Ha a vizsgalt string-gel kezdodik, hivjuk meg ujra a fuggvenyt, de redukalt loginAttempt substring-gel
             if(loginAttempt.startsWith(passSubString)) {
+                // Toroljuk ki azt a string-et a loginAttempt elejebol, amit talaltunk, keressuk meg a tobbi reszmegoldast
                 String result = passwordCracker(validPasswords, loginAttempt.substring(passSubString.length()));
+
+                // Ha ertelmezheto megoldast kapunk, fuzzuk hozza a most talalt darabkahoz az alabbi feltetelek szerint
                if(!result.equals("WRONG PASSWORD")) {
                 return passSubString + (result.isEmpty() ? "" : " " + result);
                 }
             }
            
         }
+        // Ha semmit nem talaltunk, terjunk vissza a wrong password-dal
          return "WRONG PASSWORD";
     }
 
